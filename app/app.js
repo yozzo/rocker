338bc0fce5daa9395df8da9d155c8f311e18f7c1
John <jfieldman@merchantcantos.com>
2016-03-22 17:37:20 +0000

    songs list

--- app/app.js
+++ app/app.js
@@ -16,3 +16,35 @@ App = Ember.Application.extend({
 loadInitializers(App, config.modulePrefix);
 
 export default App;
+
+//App = Ember.Application.create();
+
+var blackDog =  {
+    title: 'Black Dog',
+    band: 'Led Zeppelin',
+    rating: 3
+};
+
+var yellowLedbetter = {
+    title: 'Yellow Ledbetter',
+    band: 'Pearl Yams',
+    rating: 4
+};
+
+var pretender = {
+    title: 'The Pretender',
+    band: 'Foo Fighters',
+    rating: 2
+};
+
+var songs = [blackDog, yellowLedbetter, pretender];
+
+App.ApplicationRoute = Ember.Route.extend({
+    model: function() {
+        return songs;
+    }
+});
+
+
+
+
